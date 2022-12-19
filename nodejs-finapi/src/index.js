f6107b0ad420e7cf8f862e732aa412045924cd70
judenilson <judenilson@hotmail.com>
2022-12-19 13:37:41 -0300

    Middleware, deposit, withdraw and statement date

--- nodejs-finapi/src/index.js
+++ nodejs-finapi/src/index.js
@@ -8,6 +8,33 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  request.customer = customer; //inserindo o customer na requisição
+
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -28,15 +55,64 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/", (request, response) => {
-  const { cpf } = request.headers;
+//app.use(verifyIfExistsAccountCPF); //dessa forma tudo que estiver abaixo vai passar pelo Middleware.
 
-  const customer = customers.find((customer) => customer.cpf == cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found!" });
-  }
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.status(200).json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statmentOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statmentOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statmentOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statmentOperation);
+
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.status(200).json(statement);
 });
 
 app.listen(3333);
