e8a2d89001acdf39b04504d633543122609860c9
judenilson <judenilson@hotmail.com>
2022-12-19 12:50:29 -0300

    get statement

--- nodejs-finapi/src/index.js
+++ nodejs-finapi/src/index.js
@@ -1,4 +1,4 @@
-const { request } = require("express");
+const { request, response } = require("express");
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 
@@ -26,6 +26,17 @@ app.post("/account", (request, response) => {
     statement: [],
   });
   return response.status(201).send();
+});
+
+app.get("/statement/", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+  return response.status(200).json(customer.statement);
 });
 
 app.listen(3333);
