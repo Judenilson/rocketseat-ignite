64d07c38d4cc61b747bc14a4e065b13c31ba3069
judenilson <judenilson@hotmail.com>
2022-12-26 00:25:59 -0300

    Categories Routes update imports

--- typescript-api/src/routes/categories.routes.ts
+++ typescript-api/src/routes/categories.routes.ts
@@ -1,10 +1,8 @@
 import { request, response, Router } from "express";
-import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request,response);
