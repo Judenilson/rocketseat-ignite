2c4d866b91f8044a0cd1c35e982a8b6e5c8f2cd8
judenilson <judenilson@hotmail.com>
2022-12-26 00:26:26 -0300

    ListSpecifications done with singleton

--- typescript-api/src/routes/specifications.routes.ts
+++ typescript-api/src/routes/specifications.routes.ts
@@ -1,19 +1,15 @@
 import { request, response, Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
-import { CreateSpecificationUseCase } from "../modules/cars/useCases/createSpecification/CreateSpecificationUseCase";
+import { listSpecificationsController } from "../modules/cars/useCases/listSpecifications";
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request, response) => {
   return createSpecificationController.handle(request, response);
 });
 
 specificationsRoutes.get("/", (request, response) => {
-  const all = specificationsRepository.list();
-
-  return response.json(all);
+  return listSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
